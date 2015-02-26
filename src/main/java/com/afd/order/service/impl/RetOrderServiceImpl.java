4dd51cc4a20cc35e4b0b98c313d9ced6b7ebf54e
liubo <liubo@yiwang.com>
2015-02-26 14:11:56 +0800

    退货申请

--- src/main/java/com/afd/order/service/impl/RetOrderServiceImpl.java
+++ src/main/java/com/afd/order/service/impl/RetOrderServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.afd.common.util.DateUtils;
 import com.afd.model.order.ReturnOrder;
 import com.afd.order.dao.ReturnOrderMapper;
 import com.afd.service.order.IRetOrderService;
@@ -21,7 +22,10 @@ public class RetOrderServiceImpl implements IRetOrderService {
 
 	@Override
 	public int addRetOrder(ReturnOrder retOrder) {
-		// TODO Auto-generated method stub
+		if(retOrder!=null){
+			retOrder.setCreateDate(DateUtils.currentDate());
+			this.retOrderMapper.insertSelective(retOrder);
+		}
 		return 0;
 	}
 
